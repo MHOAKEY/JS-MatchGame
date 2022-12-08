73d19c9eaf09df9811e98fa10fedfdf6fbaae0e2
Matt Oake <matt@Matts-MacBook-Air.local>
2022-12-08 15:08:59 -0400

    Changed id's on some buttons. More if statements for id's.

--- script.js
+++ script.js
@@ -4,5 +4,28 @@ gameSpaces.forEach((gameSpace) => {
 });
 
 function showCard(event) {
-  event.target.innerHTML = "A";
+  if (event.target.id === "1") {
+    event.target.innerHTML = "!";
+  }
+  if (event.target.id === "2") {
+    event.target.innerHTML = "@";
+  }
+  if (event.target.id === "3") {
+    event.target.innerHTML = "#";
+  }
+  if (event.target.id === "4") {
+    event.target.innerHTML = "$";
+  }
+  if (event.target.id === "5") {
+    event.target.innerHTML = "%";
+  }
+  if (event.target.id === "6") {
+    event.target.innerHTML = "^";
+  }
+  if (event.target.id === "7") {
+    event.target.innerHTML = "&";
+  }
+  if (event.target.id === "8") {
+    event.target.innerHTML = "*";
+  }
 }
